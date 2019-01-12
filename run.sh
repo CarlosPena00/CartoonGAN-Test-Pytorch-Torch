49a4e2f4190022eefad1e980122d3bd28ebccd45
CarlosPena00 <chcp@cin.ufpe.br>
2019-01-11 23:16:18 -0300

    Add a small script for all styles

---
+++ run.sh
@@ -0,0 +1,4 @@
+python test.py --input_dir $1  --style Hosoda --gpu -1
+python test.py --input_dir $1  --style Hayao --gpu -1
+python test.py --input_dir $1  --style Paprika --gpu -1
+python test.py --input_dir $1  --style Shinkai --gpu -1
